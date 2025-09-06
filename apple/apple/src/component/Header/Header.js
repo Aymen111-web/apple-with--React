b7b48e58c36fd8d4542e2eadcbd054f11532c388
Aymen111-web <aymimanmoh8@gmail.com>
2025-09-06 23:10:25 +0300

    images paths are fixed and compiled

--- apple/apple/src/component/Header/Header.js
+++ apple/apple/src/component/Header/Header.js
@@ -1,42 +1,35 @@
 import React from 'react'
-import logo from '../../image/icon/logo.png';
-import search from '../../image/icon/search-icon.png';
-import cart from '../../image/icon/cart.png'
+import logo from '../../images/icons/logo.png';
+import search from '../../images/icons/search-icon.png';
+import cart from '../../images/icons/cart.png'
 
 function Header() {
   return (
-    <div className="outer-wrapper">
- <header className="header-wrapper">
-        <div className="internal-wrapper">
-          <div className="header-link-wrapper">
-            <ul>
-              <li id="logo">
-                <a href="/search/"
-                  ><img src={logo} alt=""
-                /></a>
-              </li>
-              <li><a href="#">mac</a></li>
-              <li><a href="#">iphone</a></li>
-              <li><a href="#">ipad</a></li>
-              <li><a href="#">watch</a></li>
-              <li><a href="#">tv</a></li>
-              <li><a href="#">music</a></li>
-              <li><a href="#">support</a></li>
-              <li id="search">
-                <a href="/search/"
-                  ><img src={search} alt=""
-                /></a>
-              </li>
-              <li id="cart">
-                <a href="/cart/"><img src={cart}alt="" /></a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </header>
+    <div className="outer-wrapper"> 
 
+      <header className="header-wrapper">
+			<div className="internal-wrapper"> 
+				<div className="header-links-wrapper">
+					<ul>
+						<li className="logo-link"><a href="/"><img src={logo} alt="logo"/></a></li>
+						<li><a href="/mac/">Mac</a></li>
+						<li><a href="#">iphone</a></li>
+						<li><a href="#">ipad</a></li>
+						<li><a href="#">watch</a></li>
+						<li><a href="#">tv</a></li>
+						<li><a href="#">Music</a></li>
+						<li><a href="#">Support</a></li>
+						<li className="search-link"><a href="/search/"><img src={search} alt="search"/></a></li>
+						<li className="cart-link"><a href="/cart/"><img src={cart} alt="cart" /></a></li>
 
+					</ul>					
+				</div>
+			</div>
+		</header>
     </div>
+    
+
+  
   )
 }
 export default Header;
