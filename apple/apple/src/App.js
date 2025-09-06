73bb8762ea40804964d88fe594d149672b2a8bde
Aymen111-web <aymimanmoh8@gmail.com>
2025-09-06 23:54:18 +0300

    third heighlight wrapper added

--- apple/apple/src/App.js
+++ apple/apple/src/App.js
@@ -3,6 +3,7 @@ import Header from './component/Header/Header'
 import Footer from './component/footer/Footer'
 import Mainu from './component/main/mainu'
 import Ssec from './component/second heiglight/Ssec'
+import Third from './component/third/Third'
 
 
 
@@ -12,6 +13,7 @@ function App() {
       <Header/>
       <Mainu/>
       <Ssec/>
+      <Third/>
       <Footer/>
     </div>
   );
